1cb871de31dede46674011d2a1355e05a0d73bc0
TrevorSundberg <Trevor.Sundberg@gmail.com>
2023-08-30 22:13:21 -0700

    Disable use of setjmp

--- pngerror.c
+++ pngerror.c
@@ -938,18 +938,18 @@ png_safe_execute(png_imagep image_in, int (*function)(png_voidp), png_voidp arg)
    volatile png_imagep image = image_in;
    volatile int result;
    volatile png_voidp saved_error_buf;
-   jmp_buf safe_jmpbuf;
+   //jmp_buf safe_jmpbuf;
 
    /* Safely execute function(arg) with png_error returning to this function. */
    saved_error_buf = image->opaque->error_buf;
-   result = setjmp(safe_jmpbuf) == 0;
+   //result = setjmp(safe_jmpbuf) == 0;
 
-   if (result != 0)
-   {
+   //if (result != 0)
+   //{
 
-      image->opaque->error_buf = safe_jmpbuf;
+      //image->opaque->error_buf = safe_jmpbuf;
       result = function(arg);
-   }
+   //}
 
    image->opaque->error_buf = saved_error_buf;
 
